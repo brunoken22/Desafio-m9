627851b5507efc6ca47fae26f1d08fb32d3a8f92
brunoken22 <bruno_am_22@hotmail.com>
2023-07-14 17:11:10 -0300

    cors

--- lib/middelware.ts
+++ lib/middelware.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import parseBearerToken from "parse-bearer-token";
 import { decode } from "lib/jwt";
 import NextCors from "nextjs-cors";
@@ -21,32 +21,45 @@ export function authMiddelware(callback) {
    };
 }
 
-export function handlerCors(callback) {
-   try {
-      return async function (req: NextApiRequest, res: NextApiResponse) {
-         // Run the cors middleware
-         // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
-         await NextCors(req, res, {
-            // Options
-            methods: [
-               "GET",
-               "HEAD",
-               "PUT",
-               "PATCH",
-               "POST",
-               "DELETE",
-               "OPTIONS",
-            ],
-            origin: "*",
-            optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-         });
+// export function handlerCors(callback) {
+//    try {
+//       return async function (req: NextApiRequest, res: NextApiResponse) {
+//          // Run the cors middleware
+//          // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+//          await NextCors(req, res, {
+//             // Options
+//             methods: [
+//                "GET",
+//                "HEAD",
+//                "PUT",
+//                "PATCH",
+//                "POST",
+//                "DELETE",
+//                "OPTIONS",
+//             ],
+//             origin: "*",
+//             optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+//          });
 
-         // Rest of the API logic
+//          // Rest of the API logic
 
-         callback(req, res);
-         //res.json({ message: "Hello NextJs Cors!" });
-      };
-   } catch (e) {
-      console.log(e);
-   }
+//          callback(req, res);
+//          //res.json({ message: "Hello NextJs Cors!" });
+//       };
+//    } catch (e) {
+//       console.log(e);
+//    }
+// }
+
+export function handlerCors(handler: NextApiHandler): NextApiHandler {
+   return async function nextApiHandlerWrappedWithNextCors(req, res) {
+      const methods = ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"];
+      await NextCors(req, res, {
+         methods,
+         origin: "CORS_ALLOWED_ORGIN",
+         optionsSuccessStatus: 200,
+      });
+
+      return handler(req, res);
+   };
 }
