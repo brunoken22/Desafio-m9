4a4e3b5449d29c132809b79457f77d96f6127bcd
brunoken22 <bruno_am_22@hotmail.com>
2023-07-14 20:22:20 -0300

    cors

--- lib/middelware.ts
+++ lib/middelware.ts
@@ -21,45 +21,50 @@ export function authMiddelware(callback) {
    };
 }
 
-// export function handlerCors(callback) {
-//    try {
-//       return async function (req: NextApiRequest, res: NextApiResponse) {
-//          // Run the cors middleware
-//          // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
-//          await NextCors(req, res, {
-//             // Options
-//             methods: [
-//                "GET",
-//                "HEAD",
-//                "PUT",
-//                "PATCH",
-//                "POST",
-//                "DELETE",
-//                "OPTIONS",
-//             ],
-//             origin: "*",
-//             optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-//          });
+export function handlerCors(callback) {
+   try {
+      return async function (req: NextApiRequest, res: NextApiResponse) {
+         // Run the cors middleware
+         // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+         await NextCors(req, res, {
+            // Options
+            methods: [
+               "GET",
+               "HEAD",
+               "PUT",
+               "PATCH",
+               "POST",
+               "DELETE",
+               "OPTIONS",
+            ],
+            origin: "*",
+            optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+         });
 
-//          // Rest of the API logic
+         // Rest of the API logic
+         if (req.method === "PATCH") {
+            // Agrega encabezados CORS adicionales específicos para el método PATCH
+            res.setHeader("Access-Control-Allow-Methods", "PATCH");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+         }
 
-//          callback(req, res);
-//          //res.json({ message: "Hello NextJs Cors!" });
-//       };
-//    } catch (e) {
-//       console.log(e);
-//    }
-// }
-
-export function handlerCors(handler: NextApiHandler): NextApiHandler {
-   return async function nextApiHandlerWrappedWithNextCors(req, res) {
-      const methods = ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"];
-      await NextCors(req, res, {
-         methods,
-         origin: "*",
-         optionsSuccessStatus: 200,
-      });
-
-      return handler(req, res);
-   };
+         callback(req, res);
+         //res.json({ message: "Hello NextJs Cors!" });
+      };
+   } catch (e) {
+      console.log(e);
+   }
 }
+
+// export function handlerCors(handler: NextApiHandler): NextApiHandler {
+//    return async function nextApiHandlerWrappedWithNextCors(req, res) {
+//       const methods = ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"];
+//       await NextCors(req, res, {
+//          methods,
+//          origin: "*",
+//          optionsSuccessStatus: 200,
+//       });
+
+//       return handler(req, res);
+//    };
+// }
