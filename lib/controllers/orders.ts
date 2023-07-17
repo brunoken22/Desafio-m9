3d0e9f04669268dc71766fcdbad66349273dd9ac
brunoken22 <bruno_am_22@hotmail.com>
2023-07-17 13:50:15 -0300

    prueba mercado

--- lib/controllers/orders.ts
+++ lib/controllers/orders.ts
@@ -23,7 +23,7 @@ export async function createPreferenceControllers(info, productId, token) {
          },
       ],
       back_urls: {
-         success: "https://desafio-m8-d396d.web.app",
+         success: "https://desafio-m10-weld.vercel.app/product/" + productId,
          pending: "https://desafio-m8-d396d.web.app/login",
       },
       auto_return: "approved",
