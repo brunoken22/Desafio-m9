f3a8137c7e5e8173182a76de58cf76b6320795f4
Bruno Ken <bruno_am_22@hotmail.com>
2023-10-06 21:08:02 -0300

    mejorando app

--- lib/controllers/orders.ts
+++ lib/controllers/orders.ts
@@ -1,53 +1,53 @@
-import { createPreference } from "lib/mercadopago";
-import { Order } from "lib/models/order";
-import { searchProductById } from "./products";
+import {createPreference} from 'lib/mercadopago';
+import {Order} from 'lib/models/order';
+import {searchProductById} from './products';
 export async function createPreferenceControllers(info, productId, token) {
-   const product = await searchProductById(productId as string);
+  const product = await searchProductById(productId as string);
 
-   const order = await Order.create({
-      userId: token,
-      productId: productId,
-      aditionalInfo: info,
-      status: "pending",
-   });
-   const preference = await createPreference({
-      items: [
-         {
-            title: product["Name"],
-            description: product["Name"],
-            picture_url: product["Images"][0].url,
-            category_id: "car_electronics",
-            quantity: 1,
-            currency_id: "ARS",
-            unit_price: product["Unit cost"],
-         },
-      ],
-      back_urls: {
-         success: "https://desafio-m10-weld.vercel.app/thanks",
-         pending: "https://desafio-m8-d396d.web.app/login",
+  const order = await Order.create({
+    userId: token,
+    productId: productId,
+    aditionalInfo: info,
+    status: 'pending',
+  });
+  const preference = await createPreference({
+    items: [
+      {
+        title: product['Name'],
+        description: product['Name'],
+        picture_url: product['Images'][0].url,
+        category_id: 'car_electronics',
+        quantity: info.cantidad,
+        currency_id: 'ARS',
+        unit_price: product['Unit cost'],
       },
-      auto_return: "approved",
-      external_reference: order.id,
-      notification_url:
-         "https://desafio-m9-two.vercel.app/api/webhooks/mercadopago",
-   });
-   return {
-      url: preference.init_point,
-      orderId: preference.external_reference,
-   };
+    ],
+    back_urls: {
+      success: 'https://desafio-m10-weld.vercel.app/thanks',
+      pending: 'https://desafio-m8-d396d.web.app/login',
+    },
+    auto_return: 'approved',
+    external_reference: order.id,
+    notification_url:
+      'https://desafio-m9-two.vercel.app/api/webhooks/mercadopago',
+  });
+  return {
+    url: preference.init_point,
+    orderId: preference.external_reference,
+  };
 }
 
 export async function getOrderAll(id: string) {
-   const order = await Order.searchOrder(id);
-   const data = order.map((e) => {
-      return e["_fieldsProto"];
-   });
+  const order = await Order.searchOrder(id);
+  const data = order.map((e) => {
+    return e['_fieldsProto'];
+  });
 
-   return data;
+  return data;
 }
 export async function getOrderId(id: string) {
-   const order = new Order(id);
-   await order.pull();
+  const order = new Order(id);
+  await order.pull();
 
-   return order.data;
+  return order.data;
 }
